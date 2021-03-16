857bfaef4d563070c1e8b53236d7c0a66e5d65a2
MtHappy <1275538162@qq.com>
2021-03-16 15:25:30 +0800

    1、完善Apk下载安装，无法自动更新问题

---
+++ upperdialoglib/src/main/java/com/hy/upperdialog/UpperDialogProvider.java
@@ -0,0 +1,13 @@
+package com.hy.upperdialog;
+
+import android.support.v4.content.FileProvider;
+
+import java.security.Provider;
+
+/**
+ * Author: Hrw
+ * Date: 2021/1/15 18:34
+ * Description:
+ */
+public class UpperDialogProvider extends FileProvider {
+}
